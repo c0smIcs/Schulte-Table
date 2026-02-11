b0ffd162844a8dbef3a83901c1c693bf25595672
c0smIcs <alankemov@gmail.com>
2026-02-11 20:33:11 +0300

    feat: add internal logger package using slog

---
+++ internal/logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+func InitLogger() {
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				t := a.Value.Time()
+				return slog.String(slog.TimeKey, t.Format("2006-01-02 15:04:05"))
+			}
+			return a
+		},
+	}
+
+	handler := slog.NewJSONHandler(os.Stdout, opts)
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+}
+
+func LoggerDBError(err error, dsnM string) {
+	slog.Error("не удалось подключиться к база данных",
+		slog.String("error", err.Error()),
+		slog.Group("config",
+			slog.String("DSN", dsnM),
+		),
+	)
+}
+
+func LoggerDBConnect(dsn string) {
+	slog.Info("БД успешно подключена",
+		slog.Group("config",
+			slog.String("DSN", dsn),
+		),
+	)
+}
+
+func WithSession(sessionID string) *slog.Logger {
+	return slog.With("session_id", sessionID)
+}
+
